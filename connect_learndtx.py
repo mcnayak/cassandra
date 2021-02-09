40bdb0fe2e7c71120d1f8d14722bf4b59b5685f1
Chandra <cmohan.nayak@gmail.com>
2021-02-09 15:39:25 -0800

    Read and Insert Working

--- connect_learndtx.py
+++ connect_learndtx.py
@@ -1,5 +1,8 @@
 from cassandra.cluster import Cluster
 from cassandra.auth import PlainTextAuthProvider
+from cassandra.query  import tuple_factory
+
+
 
 cloud_config= {
         'secure_connect_bundle': 'secure-connect-learndtx.zip'
@@ -7,6 +10,7 @@ cloud_config= {
 auth_provider = PlainTextAuthProvider('chandra', 'Welcome1')
 cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
 session = cluster.connect()
+session.row_factory = tuple_factory
 
 row = session.execute("select release_version from system.local").one()
 if row:
@@ -14,3 +18,20 @@ if row:
 else:
     print("An error occurred.")
 
+shows = session.execute("SELECT * FROM simpleds.movies_and_tv LIMIT 5 ")
+
+for show in shows:
+     print(show[5] + ' made ' + show[10]);
+    
+
+try:
+    session.execute("INSERT INTO simpleds.movies_and_tv (show_id,director,title) values (08022020,'Rajamouli','Baahubali')")
+
+except Exception as e:
+    print(e)
+    print('Failure')
+else:
+    print('New Row Inserted')
+finally: 
+    cluster.shutdown()
+    session.shutdown()
