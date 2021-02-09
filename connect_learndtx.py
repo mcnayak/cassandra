070a3d67fd3f71ccc5f66c0484406f42457fe6bc
Chandra <cmohan.nayak@gmail.com>
2021-02-08 18:26:49 -0800

    Conn_to_DB Working

---
+++ connect_learndtx.py
@@ -0,0 +1,16 @@
+from cassandra.cluster import Cluster
+from cassandra.auth import PlainTextAuthProvider
+
+cloud_config= {
+        'secure_connect_bundle': 'secure-connect-learndtx.zip'
+}
+auth_provider = PlainTextAuthProvider('chandra', 'Welcome1')
+cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
+session = cluster.connect()
+
+row = session.execute("select release_version from system.local").one()
+if row:
+    print(row[0])
+else:
+    print("An error occurred.")
+
