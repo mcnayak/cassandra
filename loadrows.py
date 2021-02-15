9f50c948a955da7f380aa806b650305c277be4f5
Chandra <cmohan.nayak@gmail.com>
2021-02-15 15:43:03 -0800

    Working_15Feb

--- loadrows.py
+++ loadrows.py
@@ -1,9 +1,9 @@
 from connect_learndtx import Connection
-
+import uuid
 
 try:
     connect = Connection()
-    connect.session.execute("INSERT INTO simpleds.movies_and_tv (show_id,director,title) values (09022021,'%s','%s')")
+    connect.session.execute("INSERT INTO simpleds.movies_and_tv (show_id, director,title) values (15022021,'%s','%s')")
 except Exception as e:
     print(e)
     print('Failure')
@@ -11,3 +11,8 @@ else:
     print('New Row Inserted')
 finally: 
     connect.close()
+
+
+
+#cursor.execute('SELECT * FROM salesforce.contact WHERE SFID = %(rec_id)s',{'rec_id':'00305000007YAJqAAO'})
+    
